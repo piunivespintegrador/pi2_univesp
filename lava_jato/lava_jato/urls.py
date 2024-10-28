15eb4fb83f1d411be5d0f55d59124c4418473957
AchcarLucas <lucas.campos@ambrait.com>
2024-10-28 19:48:29 -0300

    - template para o sistema de gestão

--- lava_jato/lava_jato/urls.py
+++ lava_jato/lava_jato/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('public_site.urls')),
+    path('admin/', include('private_site.urls')),
 ]
