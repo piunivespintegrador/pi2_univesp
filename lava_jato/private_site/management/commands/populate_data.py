2aba55a6d1de589bfadc551d1707fd91601fb940
AchcarLucas <lucas.campos@ambrait.com>
2024-11-09 14:51:21 -0300

    - modificação no populate_data

--- lava_jato/private_site/management/commands/populate_data.py
+++ lava_jato/private_site/management/commands/populate_data.py
@@ -50,9 +50,9 @@ class Command(BaseCommand):
 
     def create_tipo_servico(self):
         servicos = [
-            {'tipo_servico': 'Lavagem Completa', 'valor_servico': 50.00},
-            {'tipo_servico': 'Troca de Óleo', 'valor_servico': 120.00},
-            {'tipo_servico': 'Polimento', 'valor_servico': 200.00},
+            {'nome_servico': 'Lavagem Completa', 'valor_servico': 50.00},
+            {'nome_servico': 'Troca de Óleo', 'valor_servico': 120.00},
+            {'nome_servico': 'Polimento', 'valor_servico': 200.00},
         ]
 
         self.stdout.write(self.style.NOTICE(f"Criando tipo serviço"))
@@ -88,32 +88,31 @@ class Command(BaseCommand):
             self.stdout.write(f"\t{agendamento}")
 
     def create_servicos(self, n=10):
-        clientes = list(Cliente.objects.using('mysql_db').values_list('id', flat=True))
-        tipos_servico = list(TipoServico.objects.using('mysql_db').values_list('id', flat=True))
-        tipos_veiculo = list(TipoVeiculo.objects.using('mysql_db').values_list('id', flat=True))
-        formas_contato = list(FormaContato.objects.using('mysql_db').values_list('id', flat=True))
-        agendamentos = list(Agendamento.objects.using('mysql_db').values_list('id', flat=True))
-
+        clientes = Cliente.objects.using('mysql_db').all()
+        tipos_servico = TipoServico.objects.using('mysql_db').all()
+        tipos_veiculo = TipoVeiculo.objects.using('mysql_db').all()
+        formas_contato = FormaContato.objects.using('mysql_db').all()
+        agendamentos = Agendamento.objects.using('mysql_db').all()
 
         self.stdout.write(f"\t{random.choice(agendamentos)}")
-
 
         self.stdout.write(self.style.NOTICE(f"Criando serviços"))
         for _ in range(n):
             servico = Servico.objects.using('mysql_db').create(
-                agendamento_id=random.choice(agendamentos),
-                cliente_id=random.choice(clientes),
-                tipo_servico_id=random.choice(tipos_servico),
-                tipo_veiculo_id=random.choice(tipos_veiculo),
+                agendamento_id=random.choice(agendamentos).id,
+                cliente_id=random.choice(clientes).id,
+                tipo_servico_id=random.choice(tipos_servico).id,
+                tipo_veiculo_id=random.choice(tipos_veiculo).id,
                 mensagem=fake.text(),
                 valor_servico=random.uniform(50.00, 500.00)
             )
-            self.stdout.write(f"\t{servico}")
+
+            self.stdout.write(f'\t{servico}')
 
             self.stdout.write(self.style.NOTICE(f"\t * Criando serviço forma contato para o servico {servico.id}"))
             for _ in range(random.randint(1, 2)):  # associa 1 ou 2 formas de contato ao serviço
                 servico_forma_contato = ServicoFormaContato.objects.using('mysql_db').create(
                     servico_id=servico.id,
-                    forma_contato_id=random.choice(formas_contato)
+                    forma_contato_id=random.choice(formas_contato).id
                 )
                 self.stdout.write(f"\t\t{servico_forma_contato}")
