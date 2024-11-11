6faf1a499decd13fccbecec61e4154312c1d3f50
AchcarLucas <lucas.campos@ambrait.com>
2024-11-11 09:33:01 -0300

    - Unique para o Tipo Veiculo e Tipo Servico

--- lava_jato/private_site/models/tipo_servico.py
+++ lava_jato/private_site/models/tipo_servico.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TipoServico(models.Model):
-    nome_servico = models.CharField(max_length=100)
+    nome_servico = models.CharField(max_length=100, unique=True)
     valor_servico = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
--- lava_jato/private_site/models/tipo_veiculo.py
+++ lava_jato/private_site/models/tipo_veiculo.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TipoVeiculo(models.Model):
-    nome_veiculo = models.CharField(max_length=50)
+    nome_veiculo = models.CharField(max_length=50, unique=True)
 
     class Meta:
         db_table = 'tipo_veiculo'
