9cc8c40cf9a52251a28f68a9333e954c2ca7b161
AchcarLucas <lucas.campos@ambrait.com>
2024-11-11 08:39:37 -0300

    - criação das páginas de cadastramento do tipo serviço e tipo veiculo

---
+++ lava_jato/private_site/static/js/type_service/register_type_service.js
@@ -0,0 +1,74 @@
+const csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+function Reset(message)
+{
+    document.getElementById('result').textContent = '';
+    document.getElementById('result').style.color = '';
+}
+
+function Fail(message)
+{
+    document.getElementById('result').textContent = message;
+    document.getElementById('result').style.color = 'red';
+}
+
+function Warning(message)
+{
+    document.getElementById('result').textContent = message;
+    document.getElementById('result').style.color = 'yellow';
+}
+
+function Success(message)
+{
+    document.getElementById('result').textContent = message;
+    document.getElementById('result').style.color = 'green';
+}
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    Reset();
+
+    // Validação aqui
+
+    let result = submitForm(new FormData(this));
+
+    if(!result) return;
+});
+
+function submitForm(formData)
+{
+    const data = {};
+
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    let request_uri = document.getElementById('registerForm').action
+
+    // Envia os dados via AJAX (usando fetch)
+    fetch(request_uri, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrf_token
+        },
+        body: JSON.stringify(data)  // Envia os dados coletados no FormData
+    })
+    .then(response => response.json())  // Aqui você pode ajustar a resposta de acordo com sua view
+    .then(data => {
+        // Trate a resposta aqui, por exemplo, mostre uma mensagem de sucesso
+        console.info(data);
+
+        if(data.success)
+            Success(data.message);
+        else
+            Fail(data.message);
+
+        return true;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        Fail('Erro ao cadastrar o Tipo Serviço.');
+        return false;
+    });
+}
