9f6dfab98a8bcf46a5662fb70c51f1c086162ed1
AchcarLucas <lucas.campos@ambrait.com>
2024-11-11 22:06:12 -0300

    - modificação na model do cliente para aumentar o tamanho do campo e-mail

--- lava_jato/private_site/models/cliente.py
+++ lava_jato/private_site/models/cliente.py
@@ -3,10 +3,10 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     placa_carro = models.CharField(max_length=20, default="ABC1D23")
-    email_contato_1 = models.EmailField(max_length=20)
-    email_contato_2 = models.EmailField(null=True, blank=True)
-    telefone_contato_1 = models.CharField(max_length=20)
-    telefone_contato_2 = models.CharField(max_length=20, null=True, blank=True)
+    email_contato_1 = models.EmailField(max_length=200)
+    email_contato_2 = models.EmailField(max_length=200, null=True, blank=True)
+    telefone_contato_1 = models.CharField(max_length=26)
+    telefone_contato_2 = models.CharField(max_length=26, null=True, blank=True)
 
     class Meta:
         db_table = 'cliente'
