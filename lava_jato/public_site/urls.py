3c16ec3582ced4d19cbfb4ac83217f039202fd22
AchcarLucas <lucas.campos@ambrait.com>
2024-11-11 23:37:36 -0300

    - sistema de agendamento na página principal (public) finalizado

--- lava_jato/public_site/urls.py
+++ lava_jato/public_site/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     path('health/', views.health, name='health'),
+
+    path('public_register_service/', views.public_register_service, name='public_register_service'),
 ]
